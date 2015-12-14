0e3431db715b92b75b491d22f9864c87454a2956
XXMrHyde <xxmrhyde@gmx.co.uk>
2015-12-14 22:38:59 +0100

    Replace QS panel by Quick access bar, (2/2):
    Also include Slim Roms DSLV,
    "only" the base features needed for the Quick access bar arrangement,
    some additional features are disabled, e.g. the shortcut- and icon picker.

--- src/com/android/settings/darkkat/StatusBarExpandedSettings.java
+++ src/com/android/settings/darkkat/StatusBarExpandedSettings.java
@@ -16,37 +16,103 @@
 
 package com.android.settings.darkkat;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnCancelListener;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceScreen;
+import android.preference.SwitchPreference;
 import android.provider.Settings;
 import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+
+import com.android.internal.util.darkkat.SBEPanelColorHelper;
 
 import com.android.settings.InstrumentedFragment;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
 
 import java.util.Locale;
 
 public class StatusBarExpandedSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
+    private static final String PREF_CAT_QAB =
+            "status_bar_expanded_cat_qab";
+    private static final String PREF_CAT_COLORS =
+            "status_bar_expanded_cat_colors";
     private static final String PREF_QUICK_PULLDOWN =
             "status_bar_expanded_quick_pulldown";
+    private static final String PREF_SHOW_BRIGHTNESS_SLIDER =
+            "status_bar_expanded_show_brightness_slider";
+    private static final String PREF_SHOW_QAB =
+            "status_bar_expanded_show_qab";
+    private static final String PREF_BG_COLOR =
+            "status_bar_expanded_background_color";
+    private static final String PREF_ICON_COLOR =
+            "status_bar_expanded_icon_color";
+    private static final String PREF_RIPPLE_COLOR =
+            "status_bar_expanded_ripple_color";
+
+    private static final int SYSTEMUI_PRIMARY =
+            0xff263238;
+    private static final int DARKKAT_BLUE_GREY =
+            0xff1b1f23;
+    private static final int WHITE =
+            0xffffffff;
+    private static final int TRANSLUCENT_WHITE =
+            0x33ffffff;
+    private static final int HOLO_BLUE_LIGHT =
+            0xff33b5e5;
+    private static final int TRANSLUCENT_HOLO_BLUE_LIGHT =
+            0x3333b5e5;
+
+    private static final int MENU_RESET = Menu.FIRST;
+    private static final int DLG_RESET  = 0;
 
     private ListPreference mQuickPulldown;
+    private SwitchPreference mShowBrightnessSlider;
+    private SwitchPreference mShowQab;
+    private ColorPickerPreference mBackgroundColor;
+    private ColorPickerPreference mIconColor;
+    private ColorPickerPreference mRippleColor;
 
     private ContentResolver mResolver;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        refreshSettings();
+    }
+
+    public void refreshSettings() {
+        PreferenceScreen prefs = getPreferenceScreen();
+        if (prefs != null) {
+            prefs.removeAll();
+        }
 
         addPreferencesFromResource(R.xml.status_bar_expanded_settings);
         mResolver = getContentResolver();
+
+        int intColor;
+        String hexColor;
+
+        boolean showBrightnessSlider = Settings.System.getInt(mResolver,
+               Settings.System.STATUS_BAR_EXPANDED_SHOW_BRIGHTNESS_SLIDER, 1) == 1;
+        boolean showQab = Settings.System.getInt(mResolver,
+               Settings.System.STATUS_BAR_EXPANDED_SHOW_QAB, 1) == 1;
 
         mQuickPulldown =
                 (ListPreference) findPreference(PREF_QUICK_PULLDOWN);
@@ -55,9 +121,90 @@ public class StatusBarExpandedSettings extends SettingsPreferenceFragment implem
         mQuickPulldown.setValue(String.valueOf(quickPulldown));
         updateQuickPulldownSummary(quickPulldown);
         mQuickPulldown.setOnPreferenceChangeListener(this);
+
+        mShowBrightnessSlider =
+                (SwitchPreference) findPreference(PREF_SHOW_BRIGHTNESS_SLIDER);
+        mShowBrightnessSlider.setChecked(showBrightnessSlider);
+        mShowBrightnessSlider.setOnPreferenceChangeListener(this);
+
+        mShowQab =
+                (SwitchPreference) findPreference(PREF_SHOW_QAB);
+        mShowQab.setChecked(showQab);
+        mShowQab.setOnPreferenceChangeListener(this);
+
+        PreferenceCategory catQab =
+                (PreferenceCategory) findPreference(PREF_CAT_QAB);
+        PreferenceCategory catColors =
+                (PreferenceCategory) findPreference(PREF_CAT_COLORS);
+
+        if (!showQab) {
+            catQab.removePreference(findPreference("status_bar_expanded_qab_buttons"));
+        }
+
+        if (showBrightnessSlider || showQab) {
+            mBackgroundColor =
+                    (ColorPickerPreference) findPreference(PREF_BG_COLOR);
+            intColor = Settings.System.getInt(mResolver,
+                    Settings.System.STATUS_BAR_EXPANDED_BACKGROUND_COLOR,
+                    SYSTEMUI_PRIMARY); 
+            mBackgroundColor.setNewPreviewColor(intColor);
+            hexColor = String.format("#%08x", (0xffffffff & intColor));
+            mBackgroundColor.setSummary(hexColor);
+            mBackgroundColor.setDefaultColors(SYSTEMUI_PRIMARY, DARKKAT_BLUE_GREY);
+            mBackgroundColor.setOnPreferenceChangeListener(this);
+
+            mIconColor =
+                    (ColorPickerPreference) findPreference(PREF_ICON_COLOR);
+            intColor = Settings.System.getInt(mResolver,
+                    Settings.System.STATUS_BAR_EXPANDED_ICON_COLOR,
+                    WHITE); 
+            mIconColor.setNewPreviewColor(intColor);
+            hexColor = String.format("#%08x", (0xffffffff & intColor));
+            mIconColor.setSummary(hexColor);
+            mIconColor.setDefaultColors(WHITE, HOLO_BLUE_LIGHT);
+            mIconColor.setOnPreferenceChangeListener(this);
+
+            mRippleColor =
+                    (ColorPickerPreference) findPreference(PREF_RIPPLE_COLOR);
+            intColor = SBEPanelColorHelper.getRippleColor(getActivity()); 
+            mRippleColor.setNewPreviewColor(intColor);
+            hexColor = String.format("#%08x", (0xffffffff & intColor));
+            mRippleColor.setSummary(hexColor);
+            mRippleColor.setDefaultColors(TRANSLUCENT_WHITE, TRANSLUCENT_HOLO_BLUE_LIGHT);
+            mRippleColor.setOnPreferenceChangeListener(this);
+        } else {
+            catColors.removePreference(findPreference(PREF_BG_COLOR));
+            catColors.removePreference(findPreference(PREF_ICON_COLOR));
+            catColors.removePreference(findPreference(PREF_RIPPLE_COLOR));
+            removePreference(PREF_CAT_COLORS);
+        }
+
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        menu.add(0, MENU_RESET, 0, R.string.reset)
+                .setIcon(R.drawable.ic_action_reset)
+                .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case MENU_RESET:
+                showDialogInner(DLG_RESET);
+                return true;
+             default:
+                return super.onContextItemSelected(item);
+        }
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        boolean value;
+        String hex;
+        int intHex;
+
         if (preference == mQuickPulldown) {
             int intValue = Integer.valueOf((String) newValue);
             Settings.System.putInt(mResolver,
@@ -65,10 +212,129 @@ public class StatusBarExpandedSettings extends SettingsPreferenceFragment implem
                     intValue);
             updateQuickPulldownSummary(intValue);
             return true;
+        } else if (preference == mShowBrightnessSlider) {
+            value = (Boolean) newValue;
+            Settings.System.putInt(mResolver,
+                    Settings.System.STATUS_BAR_EXPANDED_SHOW_BRIGHTNESS_SLIDER,
+                    value ? 1 : 0);
+            refreshSettings();
+            return true;
+        } else if (preference == mShowQab) {
+            value = (Boolean) newValue;
+            Settings.System.putInt(mResolver,
+                    Settings.System.STATUS_BAR_EXPANDED_SHOW_QAB,
+                    value ? 1 : 0);
+            refreshSettings();
+            return true;
+        } else if (preference == mBackgroundColor) {
+            hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(mResolver,
+                    Settings.System.STATUS_BAR_EXPANDED_BACKGROUND_COLOR, intHex);
+            preference.setSummary(hex);
+            return true;
+        } else if (preference == mIconColor) {
+            hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(mResolver,
+                    Settings.System.STATUS_BAR_EXPANDED_ICON_COLOR, intHex);
+            preference.setSummary(hex);
+            return true;
+        } else if (preference == mRippleColor) {
+            hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(mResolver,
+                    Settings.System.STATUS_BAR_EXPANDED_RIPPLE_COLOR, intHex);
+            refreshSettings();
+            return true;
         }
         return false;
     }
 
+    private void showDialogInner(int id) {
+        DialogFragment newFragment = MyAlertDialogFragment.newInstance(id);
+        newFragment.setTargetFragment(this, 0);
+        newFragment.show(getFragmentManager(), "dialog " + id);
+    }
+
+    public static class MyAlertDialogFragment extends DialogFragment {
+
+        public static MyAlertDialogFragment newInstance(int id) {
+            MyAlertDialogFragment frag = new MyAlertDialogFragment();
+            Bundle args = new Bundle();
+            args.putInt("id", id);
+            frag.setArguments(args);
+            return frag;
+        }
+
+        StatusBarExpandedSettings getOwner() {
+            return (StatusBarExpandedSettings) getTargetFragment();
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            int id = getArguments().getInt("id");
+            switch (id) {
+                case DLG_RESET:
+                    return new AlertDialog.Builder(getActivity())
+                    .setTitle(R.string.reset)
+                    .setMessage(R.string.dlg_reset_values_message)
+                    .setNegativeButton(R.string.cancel, null)
+                    .setNeutralButton(R.string.dlg_reset_android,
+                            new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.STATUS_BAR_EXPANDED_QUICK_PULLDOWN, 0);
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.STATUS_BAR_EXPANDED_SHOW_BRIGHTNESS_SLIDER, 1);
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.STATUS_BAR_EXPANDED_SHOW_QAB, 1);
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.STATUS_BAR_EXPANDED_BACKGROUND_COLOR,
+                                    SYSTEMUI_PRIMARY);
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.STATUS_BAR_EXPANDED_ICON_COLOR,
+                                    WHITE);
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.STATUS_BAR_EXPANDED_RIPPLE_COLOR,
+                                    TRANSLUCENT_WHITE);
+                            getOwner().refreshSettings();
+                        }
+                    })
+                    .setPositiveButton(R.string.dlg_reset_darkkat,
+                            new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.STATUS_BAR_EXPANDED_QUICK_PULLDOWN, 1);
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.STATUS_BAR_EXPANDED_SHOW_BRIGHTNESS_SLIDER, 1);
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.STATUS_BAR_EXPANDED_SHOW_QAB, 1);
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.STATUS_BAR_EXPANDED_BACKGROUND_COLOR,
+                                    DARKKAT_BLUE_GREY);
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.STATUS_BAR_EXPANDED_ICON_COLOR,
+                                    HOLO_BLUE_LIGHT);
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.STATUS_BAR_EXPANDED_RIPPLE_COLOR,
+                                    TRANSLUCENT_HOLO_BLUE_LIGHT);
+                            getOwner().refreshSettings();
+                        }
+                    })
+                    .create();
+            }
+            throw new IllegalArgumentException("unknown id " + id);
+        }
+
+        @Override
+        public void onCancel(DialogInterface dialog) {
+
+        }
+    }
 
     private void updateQuickPulldownSummary(int value) {
         Resources res = getResources();
