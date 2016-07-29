2114a6b8d71bc09dfd141bb4e3cbcf339ee9ffdd
XXMrHyde <xxmrhyde@gmx.co.uk>
2016-07-29 20:02:08 +0200

    Status bar: Clock styles, (2/2)

--- src/com/android/settings/InstrumentedFragment.java
+++ src/com/android/settings/InstrumentedFragment.java
@@ -27,6 +27,7 @@ public abstract class InstrumentedFragment extends PreferenceFragment {
     public static final int UNDECLARED = 100000;
     public static final int BUTTONS    = 100001;
     public static final int THEME      = 100002;
+    public static final int STATUSBAR  = 100003;
 
     /**
      * Declare the view of this category.
